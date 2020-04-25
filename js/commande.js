86699402c537a40f533699a2604e23f1f2d39d70
Olivier Boucher <olive.boubou53@gmail.com>
2020-04-25 11:26:44 +0200

    envoie js

---
+++ js/commande.js
@@ -0,0 +1,34 @@
+function commande(){
+
+let data = sessionStorage.getItem('order');
+data = JSON.parse(data);
+let prix = sessionStorage.getItem('prix');
+prix = JSON.parse(prix);
+
+
+   console.log(data.orderId)
+
+    var titre = document.getElementById('title')
+    titre.textContent = 'Récapitulatif de votre commande'
+
+    let productContainer = document.getElementById("recap");
+
+    if( data != null ) {
+        productContainer.innerHTML = '';
+        Object.values(data).map( () => {
+            productContainer.innerHTML = 
+            `<p>Merci pour votre commande.</p>
+
+            <p>Celle-ci a été enregistrée sous le numéro :  ${data.orderId},
+            pour un montant total de ${prix} €.</p>
+            
+            <p>Toute l'équipe Oriteddies vous remercie de votre visite.</p>`    
+        })    
+    } else {
+        var div = document.createElement('div')
+        div.textContent = "Vous n'avez pas encore validé votre commande"
+        ourson.appendChild(div);
+    }
+}
+
+commande();
